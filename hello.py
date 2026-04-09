afaaae038cd74b4a66e46532b98dd21c480922cf
pravat panda <pravatpanda2002@gmail.com>
2026-04-09 23:34:52 +0530

    initial commit - add hello.py and readme

---
+++ hello.py
@@ -0,0 +1,7 @@
+# hello.py
+# My first file tracked with Git
+name = "Suresh"
+course = "MLOps"
+print(f"Hello! My name is {name}.")
+print(f"I am learning {course}.")
+print("This file is tracked by Git.")
